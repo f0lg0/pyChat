c40eb718e707b8d2d4d9faf270c03bba63970fb2
f0lg0 <folgonileonardo@gmail.com>
2020-01-29 21:02:57 +0100

    29-01

--- src/client.py
+++ src/client.py
@@ -3,8 +3,17 @@ import socket
 PORT = 1234
 IP = socket.gethostname()
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((IP, PORT))
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+
+client.connect(("192.168.1.34", PORT))
 print(f"Host: {IP} | Port: {PORT}")
-msg = s.recv(1024)
+msg = client.recv(1024)
 print(msg.decode("utf-8"))
+
+def send_msg():
+    to_send_msg = input(">")
+    client.send(bytes(to_send_msg, "utf-8"))
+
+while True:
+    send_msg()
--- src/server.py
+++ src/server.py
@@ -1,14 +1,39 @@
 import socket
-PORT = 1234
-IP = "192.168.1.34"
+import select
+import sys
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # socket object with socket families types --> streaming socket
-s.bind((IP, PORT))
-s.listen(5)
+HEADER_LENGTH = 10
+
+IP = "192.168.1.34"
+PORT = 1234
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # socket object with socket families types --> streaming socket
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+
+server.bind((IP, PORT))
+server.listen(10)
 
 print(f"Starting server ({IP}) on port {PORT}")
+
+
+sockets_list = [] # sockets list
+
+def receiveMsg():     
+    print("Receiving...")
+    msg = client_socket.recv(2048) 
+
+    if msg != "": 
+        # print message details
+        print("<" + address[0] + "> " + msg.decode("utf-8"))
+    else:
+        print("Empty")
+
+
 while True:
-    print("Running...")
-    clientsocket, address = s.accept()
+    client_socket, address = server.accept()
     print(f"Connection from {address} has been established!")
-    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
+    sockets_list.append(client_socket)
+    client_socket.send(bytes("Welcome to Folgo's Basement!", "utf-8"))
+
+    if sockets_list.__len__() > 1:
+        receiveMsg()
