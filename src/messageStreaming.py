83866688dbdfe1b989691459978f5e469b1362a1
JacopoFB <63126840+JacopoFB@users.noreply.github.com>
2020-04-13 09:47:31 -0600

    Add files via upload

--- src/messageStreaming.py
+++ src/messageStreaming.py
@@ -11,9 +11,8 @@ def createMsg(data):
 
 def streamData(target):
     data = target.recv(BUFFERSIZE)
-    print("recv: ", data)
-
-    if len(data) != 0: # f0lg0: TEMP FIX -> if a client disconnects the server will receive an empty msg so we skip this shit, it will return none
+    if len(data) != 0:
+        print(data)
         msglen = int(data[:BUFFERSIZE].strip())
         decoded_data = b''
 
@@ -21,14 +20,14 @@ def streamData(target):
         while len(decoded_data) < msglen:
             decoded_data += target.recv(BUFFERSIZE)
         
+        
         obj = pickle.loads(decoded_data)
 
-        # f0lg0: TEMP FIX: this is what caused your problem, after the username thing contents are already strings so you were decoding strings, currently dunno where they got set in strings. We should always keep everything in bytes tho
         try:
             obj.cont = obj.cont.decode("utf-8")
         except:
             pass
-
+        
         return obj
     
 
