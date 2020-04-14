dfa682b9e834b112190dff5dcd6ea8ccdb2bd3eb
JacopoFB <63126840+JacopoFB@users.noreply.github.com>
2020-04-14 16:46:01 -0600

    Add files via upload

--- temp/client.py
+++ temp/client.py
@@ -1,14 +1,14 @@
 import socket
-import pickle
+import json
 import threading
+import argparse
 import sys
 import time
 from datetime import datetime
 from displayBanner import displayBanner
-from msgManager import createMsg
-from msgManager import streamData
-
-HEADERSIZE = 10
+from message import Message
+from streaming import createMsg, streamData
+from dataclasses_json import dataclass_json
 
 class Client:
     def __init__(self, server_ip, port, buffer_size, client_ip):
@@ -38,14 +38,17 @@ class Client:
         while True:
             self.USERNAME = input("Enter username> ")
             if self.USERNAME:
-                #self.client.sendall("[usr]".encode("utf-8") + self.USERNAME.encode("utf-8"))
-                print(int(createMsg("[usr]" + self.USERNAME)[:HEADERSIZE].strip()))
-                self.client.sendall(createMsg("[usr]" + self.USERNAME))
+                #enc_username  = self.USERNAME.encode("utf-8")
+                packet = Message(self.CLIENT_IP, self.SERVER_IP, "temp", str(datetime.now()), self.USERNAME.encode("utf-8"), 'setuser')
 
-                check = self.client.recv(self.BUFFER_SIZE)
-                print(check.decode("utf-8"))
+                #print(packet.pack())
+                self.client.send(packet.pack())
+                
+                #check = self.client.recv(self.BUFFER_SIZE)
+                check = streamData(self.client)
+                print(check.cont.decode("utf-8"))
 
-                if check.decode("utf-8") != "Username already in use!":
+                if check.cont.decode("utf-8") != "[*] Username already in use!":
                     break
 
             else:
@@ -53,19 +56,22 @@ class Client:
 
 
     def sendMsg(self):
-        self.to_send_msg = ""
         while True:
-            self.to_send_msg = input("You> ")
+            to_send_msg = input("You> ")
 
-            if self.to_send_msg == "[export_chat]":
-                self.export = True
-            elif self.to_send_msg == "[help]":
-                self.help = True
+            if to_send_msg:
+                enc_msg = to_send_msg.encode("utf-8")
+                if to_send_msg == "[export_chat]":
+                    self.export = True
+                    packet = Message(self.CLIENT_IP, self.SERVER_IP, self.USERNAME, str(datetime.now()), enc_msg, 'export')
+                elif to_send_msg == "[help]":
+                    self.help = True
+                    packet = Message(self.CLIENT_IP, self.SERVER_IP, self.USERNAME, str(datetime.now()), enc_msg, 'help')
+                else:
+                    packet = Message(self.CLIENT_IP, self.SERVER_IP, self.USERNAME, str(datetime.now()), enc_msg, 'default')
 
-            if self.to_send_msg:
-                self.client.sendall(createMsg("> ".join([self.USERNAME, self.to_send_msg])))
-                self.to_send_msg = ""
-                self.sent = False
+                self.client.send(packet.pack())
+                to_send_msg = ""
             else:
                 print("Cant send empty message!")
 
@@ -76,9 +82,8 @@ class Client:
         iThread.start()
 
         while True:
-            print(self.client, self.BUFFER_SIZE)
-            data = streamData(self.client, self.BUFFER_SIZE)
-
+            data = streamData(self.client)
+            
             if not data:
                 print("[*] Connection closed by the server")
                 sys.exit()
@@ -86,11 +91,10 @@ class Client:
             if self.export == True:
                 timestamp = datetime.now()
                 chat_file = f"./exported/chat{str(timestamp)}.txt"
-                print(chat_file)
 
                 try:
-                    with open(chat_file, "w+") as chat:
-                        chat.write(data.decode("utf-8"))
+                    with open(chat_file, "wb+") as chat:
+                        chat.write(data.cont)
                         print("[*] Writing to file...")
 
                     print(f"[*] Finished! You can find the file at {chat_file}")
@@ -101,22 +105,45 @@ class Client:
                     print('\r' + "[*] Something went wrong" + '\n' + "You> ", end = "")
             else:
                 if self.help == True:
-                    cdict = pickle.loads(data)
-                    for command in cdict:
-                        print('\r' + command + " : " + cdict[command])
+                    for command in data:
+                        print('\r' + command + " : " + data[command])
 
                     print('\r' + "You> ", end = "")
+                    self.help = False
                 else:
-                    print('\r' + data.decode("utf-8") + '\n' + "You> ", end = "")
+                    print('\r' + data.username + "> " + data.cont.decode("utf-8") + '\n' + "You> ", end = "")
 
 
+def getArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--server", dest = "server_ip", help = "Enter server IP")
+    parser.add_argument("-p", "--port", dest = "server_port", help = "Enter server PORT")
+
+    options = parser.parse_args()
+
+    if not options.server_ip and not options.server_port:
+        raise Exception # raising exception in case the user doesn't provide values from the terminal
+
+    if not options.server_ip:
+        parser.error("*** Please specify a server IP ***")
+    elif not options.server_port:
+        parser.error("*** Please specify a port number ***")
+    else:
+        return options
 
 def main():
-    displayBanner()
+    try:
+        options = getArgs()
 
-    SERVER_IP = input("[*] Enter server's IP> ")
-    PORT = int(input("[*] Enter port> "))
-    BUFFER_SIZE = 10
+        SERVER_IP = options.server_ip
+        PORT = int(options.server_port)
+    except Exception: # in case the user doesn't provide values we ask him to enter them
+        SERVER_IP = input("*** Enter server IP address > ")
+        PORT = int(input("*** Enter server PORT number > "))
+
+    BUFFER_SIZE = 1024
+
+    displayBanner()
 
     CLIENT_IP = socket.gethostname()
 
