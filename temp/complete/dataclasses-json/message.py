31e3245575968ca3ec6fe236c551fb69d62f4322
f0lg0 <folgonileonardo@gmail.com>
2020-04-15 18:05:10 +0200

    method with dataclasses-json

---
+++ temp/complete/dataclasses-json/message.py
@@ -0,0 +1,19 @@
+import json
+
+from dataclasses import dataclass
+from dataclasses_json import dataclass_json
+
+from streaming import createMsg, streamData
+
+@dataclass_json
+@dataclass
+class Message:
+    shost: str
+    dhost: str
+    username: str # of the sender
+    date: str
+    cont: str
+    typ: str
+
+    def pack(self):
+        return createMsg(self.to_json())
