7822d2bd8938ed2d68627704905ce3f7765184a4
f0lg0 <folgonileonardo@gmail.com>
2020-04-18 20:35:51 +0200

    updated to support encryption

--- src/streaming.py
+++ src/streaming.py
@@ -1,15 +1,31 @@
+#Note that I've only modified this file, client and server remain unchanged
+
 import json
+import base64
+from encryption import AESEncryption
 
 BUFFERSIZE = 10
+PASSWORD = b'password'
+
+enc = AESEncryption(PASSWORD)
 
 # generates a message with a fixed header which specifies the length of the message (returns bytes)
 def createMsg(data):
-    finalMsg = data
+    cipher = enc.generateCipher() # everytime we generate a object, it can't be reused 
+    encrypted_data = base64.b64encode(cipher.encrypt(data.encode("utf-8"))) # base64 rappresents bytes object in strings
+    print("\nENCRYPTED DATA ", base64.b64decode(encrypted_data))
+
+
+    finalMsg = encrypted_data.decode("utf-8")
     finalMsg = f'{len(finalMsg):<10}' + finalMsg
+
+    print("\nCRAFTED ", finalMsg.encode("utf-8"))
     return finalMsg.encode("utf-8")
 
 
 def streamData(target):
+    cipher = enc.generateCipher() # everytime we generate a object, it can't be reused 
+
     data = target.recv(BUFFERSIZE)
     if len(data) != 0:
         msglen = int(data[:BUFFERSIZE].strip())
@@ -19,6 +35,13 @@ def streamData(target):
         while len(full_data) < msglen:
             full_data += target.recv(BUFFERSIZE)
 
-        return full_data # returning just the bytes, json operations done later in the code to avoid importing errors
+        print("\nRECV BASE64 ALREADY STRIPPED FROM HEADER", full_data)
+        full_data = base64.b64decode(full_data)
+        print("\nRECV BASE64 DEC ", full_data)
+
+
+        decrypted_data = cipher.decrypt(full_data)
+        
+        return decrypted_data # returning just the bytes, json operations done later in the code to avoid importing errors
     else:
         pass
