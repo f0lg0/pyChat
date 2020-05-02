9bf458370ba0de60301267b4b19d3e26d0d4e1da
JacopoFB <63126840+JacopoFB@users.noreply.github.com>
2020-05-02 13:41:45 -0600

    Cleaned up debugging logs and comments

--- src/CLIENT/encryption.py
+++ src/CLIENT/encryption.py
@@ -6,14 +6,12 @@ import base64
 class AESEncryption:
     def __init__(self, password):
         self.PASSWORD = password
-        print("* PASSWORD: ", self.PASSWORD)
 
         self.KEY = hashlib.sha256(self.PASSWORD).digest() # generating a 32 bytes key 
         
         with open('./vector', 'rb+') as f:
         	self.IV = base64.b64decode(f.read()) # initialization vector, we need to make this random. It can be shared in plain text, it's not secret
-        
-        print("VECTOR ", self.IV)
+
         self.MODE = AES.MODE_CFB # automatic padding mode (?)
 
     def generateCipher(self):
