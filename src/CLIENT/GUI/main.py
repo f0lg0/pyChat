6814f332d5196edbc5d526608a94127239926df5
JacopoFB <63126840+JacopoFB@users.noreply.github.com>
2020-05-06 09:17:46 -0600

    Updated GUI

---
+++ src/CLIENT/GUI/main.py
@@ -0,0 +1,3 @@
+import eel
+eel.init('web')
+eel.start('main.html')
---
+++ src/CLIENT/GUI/web/buttons.js
@@ -0,0 +1,35 @@
+// This Class handles inputs and their corresponding buttons
+class Input{
+	constructor(input, btn){
+		this.input = document.getElementById(input);
+		this.btn = document.getElementById(btn);
+		
+		this.initEventListeners();
+	}
+	initEventListeners(){
+		let obj = this;
+		
+		this.btn.onclick = function(){
+            //check to make sure the msg is not empty first
+			if(obj.input.value != "" && obj.input.value != null){
+				console.log(obj.input.value);
+                writeMsg(obj.input.value, "local");
+                eel.plz()
+                eel.sendMsg(obj.input.value)
+                
+				obj.clear();
+			}
+		}
+		
+		//have the enter button get "clicked" when the user presses enter
+		this.input.addEventListener("keyup", function(event) {
+			if (event.keyCode === 13) {
+				event.preventDefault();
+				obj.btn.click();
+			}
+		});
+	}
+	clear(){
+		this.input.value = '';
+	}
+}
